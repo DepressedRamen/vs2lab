2e2a6f423bd5449785d2161a6604c76558d46ae5
DepressedRamen <timruhland@protonmail.com>
2024-10-07 15:46:18 +0200

    Completed Lab1

--- lab1/clientserver.py
+++ lab1/clientserver.py
@@ -16,6 +16,30 @@ class Server:
     """ The server """
     _logger = logging.getLogger("vs2lab.lab1.clientserver.Server")
     _serving = True
+    _data=dict()
+    _data={
+        "Kevin": "123-456-7890",
+        "Tim": "456-789-0123",
+        "Chris": "789-012-3456",
+        "Maxi": "012-345-6789",
+        "Lex": "234-567-8901",
+        "Max": "345-678-9012",
+        "Tom": "456-789-0123",
+        "Hanka": "567-890-1234",
+        "Victoria": "678-901-2345",
+        "Ailana": "789-012-3456",
+        "Katrin": "890-123-4567",
+        "Karsten": "901-234-5678",
+        "Joachim": "012-345-6789",
+        "Gertrud": "123-456-7890",
+        "Moritz": "234-567-8901",
+        "David": "345-678-9012",
+        "Mohammed": "456-789-0123",
+        "Abdel": "567-890-1234",
+        "Christian": "678-901-2345",
+        "Patrick": "789-012-3456",
+        "Johannes": "890-123-4567",
+    }
 
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,6 +51,7 @@ class Server:
     def serve(self):
         """ Serve echo """
         self.sock.listen(1)
+        self._logger.info("Server started")
         while self._serving:  # as long as _serving (checked after connections or socket timeouts)
             try:
                 # pylint: disable=unused-variable
@@ -42,6 +67,33 @@ class Server:
         self.sock.close()
         self._logger.info("Server down.")
 
+    def receiveGet(self): 
+        self.sock.listen(1)
+        self._logger.info("Server started")
+        
+        while self._serving:  # as long as _serving (checked after connections or socket timeouts)
+                try:
+                    # pylint: disable=unused-variable
+                    (connection, address) = self.sock.accept()  # returns new socket and address of client
+                    while True:  # forever
+                        data = connection.recv(1024)  # receive data from client
+                        if not data:
+                            break  # stop if client stopped
+                        name = data.decode('utf-8')
+                        if name == "getall": #return complete directoy
+                            self._logger.info('GET_ALL')
+                            phone_list = []
+                            for name, number in self._data.items(): #iterate through dictionary and append to list
+                                phone_list.append(f'{name, number}')
+                            phone_list_str = "\n".join(phone_list)      
+                            connection.send(phone_list_str.encode('utf-8')) #send list as string
+                        else: #return number for given name
+                            self._logger.info(f'GET {name}')
+                            connection.send(self._data.get(name,'not found').encode('utf-8')) 
+                except socket.timeout:
+                    pass  # ignore timeouts
+        self.sock.close()
+        self._logger.info("Server down.")
 
 class Client:
     """ The client """
@@ -65,3 +117,23 @@ class Client:
     def close(self):
         """ Close socket """
         self.sock.close()
+        
+    def send(self,msg_in='Tim'):
+        """ Send message to server """
+        self.logger.info("Client sent: " + msg_in)
+        self.sock.send(msg_in.encode('utf-8')) # send message to server
+        data=self.sock.recv(1024)
+        self.logger.info("Client received: " + str(data))
+        msg_out=data.decode('utf-8') # decode message from server
+        print("\n"+msg_out+"\n") # print message
+        return msg_out
+
+    def get(self,msg_in=''):
+        """ Get number for name """
+        if msg_in=='': #if no name is given, ask for name
+            msg_in=input("Enter name: ") # ask for name 
+        return self.send(msg_in)
+    
+    def getall(self):
+        """ Get all numbers"""
+        return self.send('getall')
--- lab1/test_clientserver.py
+++ lab1/test_clientserver.py
@@ -15,7 +15,7 @@ lab_logging.setup(stream_level=logging.INFO)
 class TestEchoService(unittest.TestCase):
     """The test"""
     _server = clientserver.Server()  # create single server in class variable
-    _server_thread = threading.Thread(target=_server.serve)  # define thread for running server
+    _server_thread = threading.Thread(target=_server.receiveGet)  # define thread for running server
 
     @classmethod
     def setUpClass(cls):
@@ -25,11 +25,56 @@ class TestEchoService(unittest.TestCase):
         super().setUp()
         self.client = clientserver.Client()  # create new client for each test
 
-    def test_srv_get(self):  # each test_* function is a test
-        """Test simple call"""
-        msg = self.client.call("Hello VS2Lab")
-        self.assertEqual(msg, 'Hello VS2Lab*')
+    # def test_srv_get(self):  # each test_* function is a test
+    #     """Test simple call"""
+    #     msg = self.client.call("Hello VS2Lab")
+    #     self.assertEqual(msg, 'Hello VS2Lab*')
 
+        
+    def test_get_name(self):
+        """Test get methode"""
+        print("Search 'Tim'")
+        msg = self.client.get("Tim")
+        self.assertEqual(msg,'456-789-0123')
+        
+    def test_get_name2(self):
+        """Test get methode"""
+        print("Search 'Kevin'")
+        msg = self.client.get("Kevin")
+        self.assertEqual(msg,'123-456-7890')
+
+    def test_get_noname(self):
+        """Test get with wrong name"""
+        print("Suche 'Kare'")
+        msg = self.client.get("Kare")
+        self.assertEqual(msg,'not found')
+    
+    def test_getall(self):
+        """Test getall method"""
+        expected_msg = (
+           "('Kevin', '123-456-7890')\n"
+            "('Tim', '456-789-0123')\n"
+            "('Chris', '789-012-3456')\n"
+            "('Maxi', '012-345-6789')\n"
+            "('Lex', '234-567-8901')\n"
+            "('Max', '345-678-9012')\n"
+            "('Tom', '456-789-0123')\n"
+            "('Hanka', '567-890-1234')\n"
+            "('Victoria', '678-901-2345')\n"
+            "('Ailana', '789-012-3456')\n"
+            "('Katrin', '890-123-4567')\n"
+            "('Karsten', '901-234-5678')\n"
+            "('Joachim', '012-345-6789')\n"
+            "('Gertrud', '123-456-7890')\n"
+            "('Moritz', '234-567-8901')\n"
+            "('David', '345-678-9012')\n"
+            "('Mohammed', '456-789-0123')\n"
+            "('Abdel', '567-890-1234')\n"
+            "('Christian', '678-901-2345')\n"
+            "('Patrick', '789-012-3456')\n"
+            "('Johannes', '890-123-4567')"
+        )
+        
     def tearDown(self):
         self.client.close()  # terminate client after each test
 
