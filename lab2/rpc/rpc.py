2f11c902a54c63b8d626dc748b09c435f6cc1c88
DepressedRamen <timruhland@protonmail.com>
2024-10-08 15:34:37 +0200

    finished lab2

--- lab2/rpc/rpc.py
+++ lab2/rpc/rpc.py
@@ -1,4 +1,6 @@
 import constRPC
+import threading
+import time
 
 from context import lab_channel
 
@@ -25,13 +27,40 @@ class Client:
     def stop(self):
         self.chan.leave('client')
 
-    def append(self, data, db_list):
+    def append(self, data, db_list, callback):
         assert isinstance(db_list, DBList)
         msglst = (constRPC.APPEND, data, db_list)  # message payload
         self.chan.send_to(self.server, msglst)  # send msg to server
-        msgrcv = self.chan.receive_from(self.server)  # wait for response
-        return msgrcv[1]  # pass it to caller
+        
+        msgrcv_ack = self.chan.receive_from(self.server) # type: ignore
+        print("Ergebnis der Ack-Antwort: ", msgrcv_ack[1])
 
+        thread = AsyncAppend(self.chan, self.server, callback) #make append obj.
+        thread.start()      #start thread
+        i = 0 
+        while (not thread.done): #printing output to show that the client is still active
+            print("client is waiting")
+            time.sleep(1)
+        thread.join()            
+        
+        """msgrcv = self.chan.receive_from(self.server)  # wait for response
+        return msgrcv[1]  # pass it to caller"""
+
+class AsyncAppend(threading.Thread):
+    def __init__(self, chan, server, callback):
+        threading.Thread.__init__(self)
+        self.chan = chan
+        self.server = server
+        self.done = False
+        self.callback = callback
+
+    def run(self):
+        print('asynch_append start receive')
+        # wait for response --> Hier Thread
+        msgrcv = self.chan.receive_from(self.server) # receive response
+        print('asynch_append receive done') 
+        self.done = True
+        self.callback(msgrcv[1]) # call callback function
 
 class Server:
     def __init__(self):
@@ -51,7 +80,14 @@ class Server:
             if msgreq is not None:
                 client = msgreq[0]  # see who is the caller
                 msgrpc = msgreq[1]  # fetch call & parameters
-                if constRPC.APPEND == msgrpc[0]:  # check what is being requested
+                # check what is being requested
+                if constRPC.APPEND == msgrpc[0]:
+                    # server received request
+                    self.chan.send_to({client}, "acknowledge")
+                    # wait 10 seconds to simulate prolonged processing time
+                    for i in range(10):
+                        print("Server is working")
+                        time.sleep(1)
                     result = self.append(msgrpc[1], msgrpc[2])  # do local call
                     self.chan.send_to({client}, result)  # return response
                 else:
--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -3,14 +3,17 @@ import logging
 
 from context import lab_logging
 
+def callback(msg):
+    print("Ergebnis in Callback-Funktion")
+    print("Inhalt der Nachricht :" ,msg.value)
+
 lab_logging.setup(stream_level=logging.INFO)
 
 cl = rpc.Client()
 cl.run()
 
 base_list = rpc.DBList({'foo'})
-result_list = cl.append('bar', base_list)
-
-print("Result: {}".format(result_list.value))
-
+cl.append('bar', base_list, callback)
 cl.stop()
+
+
